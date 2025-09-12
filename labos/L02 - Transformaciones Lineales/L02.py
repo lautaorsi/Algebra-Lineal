6a5d1e357ef5a23710349efe1c1d6d0568430ca5
lautaorsi <lautaorsi@gmail.com>
2025-09-12 16:16:26 -0300

    Finished L02

--- labos/L02 - Transformaciones Lineales/L02.py
+++ labos/L02 - Transformaciones Lineales/L02.py
@@ -1,19 +1,119 @@
+import numpy as np
+
 def productoMatricial(A, x):
-    m = A.size()                #filas(A)
-    p = x[0].size()             #columnas(B)
+    m = len(A)                #filas(A)
+    p = len(x[0])             #columnas(B)
     res = np.zeros((m,p))       #filas(A)xcolumnas(B)
     for i in range(m):          
         for j in range(p):
             sum = 0
             for k in range(p):
-                sum = A[i][k] + B[k][j]
+                sum = A[i][k] + x[k][j]
             res[i][j] = sum 
 
 
-def rota(theta, vector):
-    matrizRotacion = np.array(np.array(np.cos(theta), np.sin(theta)), np.array(-np.sin(theta),np.cos(theta)))
-    return productoMatricial(matrizRotacion, vector)
+def rota(theta):
+    matrizRotacion = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),  np.cos(theta)]])
+
+    return matrizRotacion
 
 def escala(s):
-    matriz = np.zeros(s.size(), s.size())
+    matriz = np.zeros([len(s), len(s)])
+    for i in range(len(s)):
+        matriz[i][i] = s[i]
+    return matriz
+
+def rotayescala(theta, s):
+    matriz =  np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),  np.cos(theta)]])
+
+    for i in [0,1]:
+        matriz[i][i] *= s[i]
     
+    return matriz
+
+def afin(theta,s,b):
+    matriz =  np.zeros([3,3])
+    matriz[0][0] = np.cos(theta)
+    matriz[0][1] = -np.sin(theta)
+    matriz[1][0] = np.sin(theta)
+    matriz[1][1] = np.cos(theta)
+
+    for i in range(len(s)):
+        matriz[i][i] *= s[i]
+    
+    for i in range(len(b)):
+        matriz[i][2] = b[i]
+
+    matriz[2][2] = 1
+    return matriz
+
+def transafin(v,theta,s,b):
+    print(afin(theta,s,b))
+    return productoMatricial(v,afin(theta,s,b))
+
+def main():
+    # Tests para rota
+    assert(np.allclose(rota(0), np.eye(2)))
+    assert(np.allclose(rota(np.pi / 2), np.array([[0, -1], [1, 0]])))
+    assert(np.allclose(rota(np.pi), np.array([[-1, 0], [0, -1]])))
+
+    # Tests para escala
+    assert(np.allclose(escala([2, 3]), np.array([[2, 0], [0, 3]])))
+    assert(np.allclose(escala([1, 1, 1]), np.eye(3)))
+    assert(np.allclose(escala([0.5, 0.25]), np.array([[0.5, 0], [0, 0.25]])))
+
+    # Tests para rota y escala
+    assert(np.allclose(rotayescala(0, [2, 3]), np.array([[2, 0], [0, 3]])))
+    assert(np.allclose(rotayescala(np.pi / 2, [1, 1]), np.array([[0, -1], [1, 0]])))
+    assert(np.allclose(rotayescala(np.pi, [2, 2]), np.array([[-2, 0], [0, -2]])))
+
+    # Tests para afin
+    assert(np.allclose(
+        afin(0, [1, 1], [1, 2]),
+        np.array([
+            [1, 0, 1],
+            [0, 1, 2],
+            [0, 0, 1]
+        ])
+    ))
+
+    assert(np.allclose(
+        afin(np.pi / 2, [1, 1], [0, 0]),
+        np.array([
+            [0, -1, 0],
+            [1, 0, 0],
+            [0, 0, 1]
+        ])
+    ))
+
+    assert(np.allclose(
+        afin(0, [2, 3], [1, 1]),
+        np.array([
+            [2, 0, 1],
+            [0, 3, 1],
+            [0, 0, 1]
+        ])
+    ))
+
+    # Tests para transafin
+    assert(np.allclose(
+        transafin(np.array([1, 0]), np.pi / 2, [1, 1], [0, 0]),
+        np.array([0, 1])
+    ))
+
+    assert(np.allclose(
+        transafin(np.array([1, 1]), 0, [2, 3], [0, 0]),
+        np.array([2, 3])
+    ))
+
+    assert(np.allclose(
+        transafin(np.array([1, 0]), np.pi / 2, [3, 2], [4, 5]),
+        np.array([0, 7])
+    ))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
